1c0eae4078ee2d542e2c2f9579e243a7a83c675e
Marsha Annon <marshaannon@gmail.com>
2020-03-06 00:31:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(katz_deli)
+  array = ["The line is currently:"]
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |index, name|
+      array << "#{index+1}. #{name}"
+    end
+  end
+    puts array.join(" ")
+end
+
+def take_a_number(katz_deli, names)
+  katz_deli.each_with_index do |index, name|
+    katz_deli.push("#{index}. #{name}")
+  end
+  if katz_deli.size + 0
+    puts "The line is: #{katz_deli.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
